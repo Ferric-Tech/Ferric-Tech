6f98d0c2fedf850a43d4d361e66502a04a0d8197
Terric-Tech <ferrictech@gmail.com>
2022-06-28 11:31:09 +0200

    Generating carousel component

--- website/src/app/app.module.ts
+++ website/src/app/app.module.ts
@@ -7,9 +7,10 @@ import { HomeComponent } from './pages/home/home.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
+import { CarouselComponent } from './components/carousel/carousel.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, ProjectsComponent],
+  declarations: [AppComponent, HomeComponent, ProjectsComponent, CarouselComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
