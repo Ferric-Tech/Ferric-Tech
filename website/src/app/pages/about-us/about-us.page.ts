77ba01d7312ed0f798fb5b2926d71deb24b7ff29
Terric-Tech <ferrictech@gmail.com>
2022-06-28 16:10:24 +0200

    Moved about us page content to us plain text screen

--- website/src/app/pages/about-us/about-us.page.ts
+++ website/src/app/pages/about-us/about-us.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  PlainTextScreen,
+  PlainTextScreenConfig,
+} from 'src/app/screens/plain-text/plain-text.screen';
 
 @Component({
   selector: 'app-about-us',
@@ -6,6 +10,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about-us.page.scss'],
 })
 export class AboutUsPage implements OnInit {
+  screenConfig = {
+    screenTitle: 'About us',
+    paragraphs: [
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+    ],
+  } as PlainTextScreenConfig;
+
   constructor() {}
 
   ngOnInit(): void {}
--- website/src/app/screens/plain-text/plain-text.screen.ts
+++ website/src/app/screens/plain-text/plain-text.screen.ts
@@ -1,11 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+export interface PlainTextScreenConfig {
+  screenTitle: string;
+  paragraphs: string[];
+}
 
 @Component({
-  selector: 'app-plain-text',
+  selector: 'app-plain-text-screen',
   templateUrl: './plain-text.screen.html',
   styleUrls: ['./plain-text.screen.scss'],
 })
 export class PlainTextScreen implements OnInit {
+  @Input() config = {} as PlainTextScreenConfig;
   constructor() {}
 
   ngOnInit(): void {}
