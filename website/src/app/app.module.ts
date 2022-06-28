c46f494ffdc1ef283557923611eaeaa0865eda81
Terric-Tech <ferrictech@gmail.com>
2022-06-28 18:04:41 +0200

    Generate CarouselScreen

--- website/src/app/app.module.ts
+++ website/src/app/app.module.ts
@@ -2,20 +2,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 // Angualar material imports
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 
-// Components
-import { AppComponent } from './app.component';
+// Components - Pages
 import { HomePage } from './pages/home/home.page';
 import { ProjectsPage } from './pages/projects/projects.page';
-import { CarouselComponent } from './components/carousel/carousel.component';
 import { AboutUsPage } from './pages/about-us/about-us.page';
 import { ContactUsPage } from './pages/contact-us/contact-us.page';
+
+// Components - Screens
 import { PlainTextScreen } from './screens/plain-text/plain-text.screen';
+import { CarouselScreen } from './screens/carousel/carousel.screen';
+
+// Components - Components
+import { CarouselComponent } from './components/carousel/carousel.component';
 
 @NgModule({
   declarations: [
@@ -26,6 +31,7 @@ import { PlainTextScreen } from './screens/plain-text/plain-text.screen';
     AboutUsPage,
     ContactUsPage,
     PlainTextScreen,
+    CarouselScreen,
   ],
   imports: [
     BrowserModule,
---
+++ website/src/app/screens/carousel/carousel.screen.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-carousel-screen',
+  templateUrl: './carousel.screen.html',
+  styleUrls: ['./carousel.screen.scss'],
+})
+export class CarouselScreen implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
