118c48cbbccce0b03aa935c0b96ffabcc2fd222a
Terric-Tech <ferrictech@gmail.com>
2022-06-28 11:39:37 +0200

    Moved carousel code from home to carousel component

--- website/src/app/components/carousel/carousel.component.ts
+++ website/src/app/components/carousel/carousel.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+export interface CarouselOption {
+  title: string;
+}
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.scss']
+  styleUrls: ['./carousel.component.scss'],
 })
 export class CarouselComponent implements OnInit {
+  @Input() menu = [] as CarouselOption[];
 
-  constructor() { }
+  menuOption = 0;
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  arrowClicked(goForward: boolean) {
+    if (goForward && this.menuOption + 1 === this.menu.length) {
+      this.menuOption = 0;
+      return;
+    }
+    if (!goForward && this.menuOption === 0) {
+      this.menuOption = this.menu.length - 1;
+      return;
+    }
+    this.menuOption = goForward ? this.menuOption + 1 : this.menuOption - 1;
   }
-
 }
--- website/src/app/pages/home/home.component.ts
+++ website/src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CarouselOption } from 'src/app/components/carousel/carousel.component';
 
 @Component({
   selector: 'app-home',
@@ -6,22 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  menu = ['Projects', 'About us', 'Get in touch'];
-  menuOption = 0;
+  menu: CarouselOption[] = [
+    { title: 'Projects' },
+    { title: 'About us' },
+    { title: 'Get in touch' },
+  ];
 
   constructor() {}
 
   ngOnInit(): void {}
-
-  arrowClicked(goForward: boolean) {
-    if (goForward && this.menuOption + 1 === this.menu.length) {
-      this.menuOption = 0;
-      return;
-    }
-    if (!goForward && this.menuOption === 0) {
-      this.menuOption = this.menu.length - 1;
-      return;
-    }
-    this.menuOption = goForward ? this.menuOption + 1 : this.menuOption - 1;
-  }
 }
