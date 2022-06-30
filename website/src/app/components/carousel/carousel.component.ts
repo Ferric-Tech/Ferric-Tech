e4790dbea54f1fcf5c7a32ab0cba41d240a0bb01
Terric-Tech <ferrictech@gmail.com>
2022-06-30 15:32:06 +0200

    Carousel now able to handle multiple tiles for desktop view

--- website/src/app/components/carousel/carousel.component.ts
+++ website/src/app/components/carousel/carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, HostListener, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Button, ButtonType } from 'src/app/interfaces/widgets.interface';
 
@@ -15,13 +15,39 @@ export interface CarouselOption {
 })
 export class CarouselComponent implements OnInit {
   @Input() options = [] as CarouselOption[];
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.setNumberOfTiles();
+  }
 
   buttonType = ButtonType;
   menuOption = 0;
+  numberOfTiles: number[] = [];
 
   constructor(private router: Router) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.setNumberOfTiles();
+  }
+
+  private setNumberOfTiles() {
+    if (window.innerWidth < 900) {
+      this.setImageIndexArray(1);
+      return;
+    }
+    if (window.innerWidth < 1500) {
+      this.setImageIndexArray(3);
+      return;
+    }
+    this.setImageIndexArray(5);
+  }
+
+  private setImageIndexArray(n: number) {
+    this.numberOfTiles = [];
+    for (let i = 1; i < Math.min(n, this.options.length) + 1; i++) {
+      this.numberOfTiles.push(i);
+    }
+  }
 
   onArrowClick(goForward: boolean) {
     if (goForward && this.menuOption + 1 === this.options.length) {
